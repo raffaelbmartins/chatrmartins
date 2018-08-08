f638c1ed26f5bd98b67e2bb8895b03211d86ed5d
Raffael Barbosa Martins <raffael.martins@receita.pb.gov.br>
2018-08-08 16:14:43 -0300

    Fix error environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   token: "T*=/:4Z7,e8hF3vgxqS$]rVrf4:]uL:aGt;*Dk!w'RJ>&NXAJ,^}F5RIfpbR[YF",
-  urlGet: 'http:/www.seuprofissional.com.br/chat/mensagens',
-  urlSave: 'http:/www.seuprofissional.com.br/chat/conversas/salvar',
-  origin: 'http:/www.seuprofissional.com.br'
+  urlGet: 'http://www.seuprofissional.com.br/chat/mensagens',
+  urlSave: 'http://www.seuprofissional.com.br/chat/conversas/salvar',
+  origin: 'http://www.seuprofissional.com.br'
 };
